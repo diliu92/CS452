b18485b2ae13410161e72d56b420c4da24ca61b6
y363liu@gmail.com <y363liu@y363liu-VirtualBox.(none)>
2014-05-17 16:52:03 -0400

    Yang_change_V22

--- include/scheduler.h
+++ include/scheduler.h
@@ -2,7 +2,13 @@
 #define __SCHEDULER_H__
 
 #include <kernel.h>
- 
+#include <task.h>
+
+ struct priorityQueue{
+	struct task* head;	
+	struct task* tail;		
+}; 
+
 struct task* getNextTask(struct kernelGlobal*);
 
 void pushQueue(struct kernelGlobal* kernelData, int qIdx, struct task* tsk);
--- src/kernel/task.c
+++ src/kernel/task.c
@@ -4,7 +4,7 @@
  * priority(1 to 16, inclusive)
  */ 
 
-int CreateTask(int priority, void (*code)(),){
+int CreateTask(int priority, void (*code)()){
 	return 0;
 	
 }
