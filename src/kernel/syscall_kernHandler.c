e602cc0dff6e02b7250a44d49a7672216f49d767
y363liu@gmail.com <y363liu@y363liu-VirtualBox.(none)>
2014-05-19 21:17:02 -0400

    Yang_Change

--- src/kernel/syscall_kernHandler.c
+++ src/kernel/syscall_kernHandler.c
@@ -10,6 +10,18 @@ int syscall_kernHandler(kernGlobal* kernelData, requestMessage* req){
 		case SYSCALL_CREATE:
 			return Task_create(kernelData,
 				((requestMessage_Create*)req)->priority, ((requestMessage_Create*)req)->code	);		
+		case SYSCALL_MY_TID:
+			return Task_create(kernelData,
+				((requestMessage_Create*)req)->priority, ((requestMessage_Create*)req)->code	);	
+		case SYSCALL_MY_PARENT_TID:
+			return Task_create(kernelData,
+				((requestMessage_Create*)req)->priority, ((requestMessage_Create*)req)->code	);	
+		case SYSCALL_PASS:
+			return Task_create(kernelData,
+				((requestMessage_Create*)req)->priority, ((requestMessage_Create*)req)->code	);	
+		case SYSCALL_EXIT:
+			return Task_create(kernelData,
+				((requestMessage_Create*)req)->priority, ((requestMessage_Create*)req)->code	);	
 	}
 	
 	
