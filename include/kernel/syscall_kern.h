4f82ce83f2ad8325e7b508ac6d53e48ce50168fd
y363liu@gmail.com <y363liu@y363liu-VirtualBox.(none)>
2014-05-19 20:55:19 -0400

    Yang_Change

--- include/kernel/syscall_kern.h
+++ include/kernel/syscall_kern.h
@@ -25,46 +25,10 @@ int Task_create(int priority, void (*code)());
  */ 
 
 
-static task* Scheduler_popQueue(kernGlobal* kernelData, int qIdx){
-	priorityQueue* qItem = &((kernelData->priorityQueues)[qIdx]);
-	
-	task* retval = qItem->head;
-	
-	qItem->head = (retval)->nextTask;
-	
-	(retval)->nextTask = NULL;
-	/*
-	 * EQC(Empty Queue Check)
-	 */ 
-	if(qItem->head == NULL && qItem->tail == retval){	
-		qItem->tail = NULL;
-	}
-			
-	return retval;
-}
-
-task* Scheduler_getNextReadyTask(kernGlobal* kernelData){	
-	int i = Scheduler_findNextPriorityQueue(kernelData);
-	
-	if (i >= 0)
-		return Scheduler_popQueue(kernelData, i);
-			
-	return NULL;
-}
 
 
-void Scheduler_pushQueue(kernGlobal* kernelData, int qIdx, task* tsk){
-	priorityQueue* qItem = &((kernelData->priorityQueues)[qIdx]);
+task* Scheduler_getNextReadyTask(kernGlobal* kernelData);
 
-	if (Scheduler_isQueueEmpty(kernelData, qIdx)){
-			qItem->head=tsk;
-			qItem->tail=tsk;
-	}
-	else{
-			(qItem->tail)->nextTask = tsk;
-			qItem->tail = (qItem->tail)->nextTask;
-	}
-}
 
 
 
