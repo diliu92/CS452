dfa6774b9566e22bb3bff3e0bd0c484d82005d1b
y363liu@gmail.com <y363liu@y363liu-VirtualBox.(none)>
2014-05-21 16:43:13 -0400

    Yang_Change

--- include/user.h
+++ include/user.h
@@ -2,6 +2,7 @@
 #define __USER_H__
 
 #include <user/syscall_userHandler.h>
+#include <user/first_user_task.h>
 
 #include <common/syscall_information.h>
 
--- include/user/first_user_task.h
+++ include/user/first_user_task.h
@@ -3,9 +3,6 @@
 
 
 
-
-
-
 void
 kernelPartOne();
 
