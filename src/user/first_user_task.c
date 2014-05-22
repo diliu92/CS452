64430542c708e5401c27ad66c804f18d48703c03
y363liu@gmail.com <y363liu@y363liu-VirtualBox.(none)>
2014-05-21 21:43:35 -0400

    Yang_Change

--- src/user/first_user_task.c
+++ src/user/first_user_task.c
@@ -19,10 +19,10 @@ void
 kernelPartOne(){	//priority of FUT is 3
 	bwprintf( COM2, "First:entering\r\n" );
 	
-	int Tid_low1 = Create(4,kernelPartOne_code);
-	int Tid_low2 = Create(4,kernelPartOne_code);
-	int Tid_high1 = Create(2,kernelPartOne_code);
-	int Tid_high2 = Create(2,kernelPartOne_code);	
+	int Tid_low1 = Create(4,&kernelPartOne_code);
+	int Tid_low2 = Create(4,&kernelPartOne_code);
+	int Tid_high1 = Create(2,&kernelPartOne_code);
+	int Tid_high2 = Create(2,&kernelPartOne_code);	
 	
 	bwprintf( COM2, "First:exiting\r\n" );
 	
