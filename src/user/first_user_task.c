8f4bac0245ce6cf2b7cd11f480c10798ef608fb0
y363liu@gmail.com <y363liu@y363liu-VirtualBox.(none)>
2014-05-22 14:04:04 -0400

    Yang_Change

--- src/user/first_user_task.c
+++ src/user/first_user_task.c
@@ -4,9 +4,9 @@
 
 void
 kernelPartOne_code(){
-	//bwprintf( COM2, "My TID: %u, My Parent TID:%u\r\n", MyTid(), MyParentTid());
-	//Pass();
-	//bwprintf( COM2, "My TID: %u, My Parent TID:%u\r\n", MyTid(), MyParentTid());
+	bwprintf( COM2, "My TID: %u, My Parent TID:%u\r\n", MyTid(), MyParentTid());
+	Pass();
+	bwprintf( COM2, "My TID: %u, My Parent TID:%u\r\n", MyTid(), MyParentTid());
 	bwprintf( COM2, "kernelPartOne_code\r\n" );	
 	Exit();	
 }
@@ -20,12 +20,13 @@ void
 kernelPartOne(){	//priority of FUT is 3
 	bwprintf( COM2, "First:entering\r\n" );
 	bwprintf( COM2, "First:middle\r\n" );
-	//int Tid_low1 = Create(4,kernelPartOne_code);
-	//int Tid_low2 = Create(4,kernelPartOne_code);
-	//int Tid_high1 = Create(2,kernelPartOne_code);
-	//int Tid_high2 = Create(2,kernelPartOne_code);	
 	
-	Pass();
+	int Tid_low1 = Create(4,kernelPartOne_code);
+	int Tid_low2 = Create(4,kernelPartOne_code);
+	int Tid_high1 = Create(2,kernelPartOne_code);
+	int Tid_high2 = Create(2,kernelPartOne_code);	
+	
+	//Pass();
 	
 	//MyTid();
 	
