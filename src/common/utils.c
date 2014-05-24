9db91e5b4586373dc573be980bd5f705386a5bf8
y363liu@gmail.com <y363liu@y363liu-VirtualBox.(none)>
2014-05-24 19:39:55 -0400

    Yang_Change

--- src/common/utils.c
+++ src/common/utils.c
@@ -5,15 +5,12 @@ void*
 memcpy(const char* src, char* dest, int size){
 	while(size--)
 		*dest++ = *src++;
-	
 	return dest;
 }
 
 int
-strlen(const char* str){
-	char* s;
-	
-	for (s = str; *s; s++);
-	
+strlen(const char* str){	
+	char* s;	
+	for (s = str; *s; ++s);	
 	return s - str;	
 }
