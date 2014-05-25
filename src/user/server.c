86072d220252b604650c832d8bc2c8ffc0b8cb9f
y363liu@gmail.com <y363liu@y363liu-VirtualBox.(none)>
2014-05-25 16:15:18 -0400

    Yang_Change

--- src/user/server.c
+++ src/user/server.c
@@ -47,7 +47,7 @@ nameServer(){
 			{
 				// error checking to be added
 				
-				nameServerEntry* entry = &(nameServerData->entries[req.tid]);
+				nameServerEntry* entry = &(nameServerData.entries[req.tid]);
 				nameEntry* name = &(entry->names[entry->nextSpot])
 				
 				name->size = req.size;
