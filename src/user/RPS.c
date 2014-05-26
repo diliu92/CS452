2e21f6801178067f9213d07c5d54e1f934f3c495
y363liu@gmail.com <y363liu@y363liu-VirtualBox.(none)>
2014-05-26 12:29:22 -0400

    Yang_Change

--- src/user/RPS.c
+++ src/user/RPS.c
@@ -15,18 +15,53 @@ typedef struct clientPair{
 }clientPair;
 
 typedef struct RPS{
-	
+	clientPair clientPairs[MAX_PAIR_OF_CLIENTS];
 }RPS;
 
 
 static void
-RPS_ServerInit(){
+RPS_ServerInit(RPS* rps){
+	int i;
+	clientPair* pair;
+	
+	for (i = 0; i < MAX_PAIR_OF_CLIENTS; i++)
+	{
+		pair = &(rps->clientPairs[i])
+		
+		pair->clientOne = -1;
+		pair->clientTwo = -1;
+		
+		pair->clientOneChoice = IDLE;
+		pair->clientTwoChoice = IDLE;
+		
+		pair->status = WAITING;		
+	}
 }
 
 
 
 void
 RPS_Server(){
+	RPS rps;
+	RPS_ServerInit(&rps);
 	
-	
+	int playerTid;
+	RPS_clientRequest req;
+
+	for(;;){
+		Receieve(&playerTid, &req, sizeof(RPS_clientRequest));
+		switch (req.rpscall_uid)
+		{
+			case RPSCALL_SIGNUP:
+				
+				break;
+			case RPSCALL_PLAY:
+				
+				break;	
+			case RPSCALL_QUIT:
+				
+				break;					
+		}
+		
+	}
 }
