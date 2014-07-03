13b0518c21bd929815dbbe8f8f7f05e2d2a2e509
y363liu@gmail.com <y363liu@y363liu-VirtualBox.(none)>
2014-07-03 13:13:42 -0400

    YangChange

--- src/user/first_user_task.c
+++ src/user/first_user_task.c
@@ -26,7 +26,7 @@ idleTask(){
 			idleTime += currentTime - lastTime;
 		
 		if( totalTime >= printInterval){
-			sprintf( COM2, "%s\033[2;0H%s%sIdleTask: %d%%%s%s", save, clearLine, green, (100*idleTime)/totalTime, restore, resetColor);
+			sprintf( COM2, "%s\033[0;0H%s%d%%%s%s", save, yellow, (100*idleTime)/totalTime, restore, resetColor);
 			
 			totalTime = 0;
 			idleTime  = 0;
