88e64139a9bccc6149c96c0a34f111d1354a5925
y363liu@gmail.com <y363liu@y363liu-VirtualBox.(none)>
2014-07-10 19:19:54 -0400

    YangChange

--- include/train/routeServer.h
+++ include/train/routeServer.h
@@ -1,24 +1,37 @@
-// #ifndef __ROUTESERVER_H__
-// #define __ROUTESERVER_H__
+#ifndef __ROUTESERVER_H__
+#define __ROUTESERVER_H__
 
-// #define ROUTESERVER_TID					11
+#define ROUTESERVER_TID					11
 
-// #define ROUTESERVER_PREDICT_NEXT_SENSOR		0
+#define ROUTESERVER_ROUTE_GET_SHORTEST		0		
 
-// typedef struct routeServerRequest{		
-// 	int 	rtSvrReq_uid;
+#define ROUTESERVER_TRACKNODE_CAPTURE		1		//for now, dont need this call 'yet'
+#define ROUTESERVER_TRACKNODE_RELEASE		2
+
+#define TRACK_MAX_NUM 144
+
+
+typedef struct routeServerRequest{		
+	int 	rtSvrReq_uid;
+	int		retval;
 	
-// 	int 	curValue;
-// 	int 	destValue;
-// 	int 	ts;
-// }trackServerRequest;
+	int 	src;
+	int 	dest;
+}routeServerRequest;
 
-// typedef struct nextSensorInfo{
-// 	char *nextSensorLabel;
-// 	int dist;
-// 	int expectedTime;
-// }nextSensorInfo;
+typedef struct routeServerResponse_Path{
+	int		path[TRACK_MAX_NUM];
+}routeServerResponse_Path;
 
-// nextSensorInfo
-// predictNextSensor(int curSensor, int ts);
-// #endif
+
+
+
+typedef struct nextSensorInfo{
+	char *nextSensorLabel;
+	int dist;
+	int expectedTime;
+}nextSensorInfo;
+
+nextSensorInfo
+predictNextSensor(int curSensor, int ts);
+#endif
--- src/train/routeServer.c
+++ src/train/routeServer.c
@@ -1,67 +1,229 @@
-// #include <train.h>
+#include <train.h>
 
-// void
-// routeServer(){
-// 	track_node trackA[TRACK_MAX];
-// 	init_tracka(trackA);
 
-// 	int requester;
-// 	routeServerRequest req;
+#define	FREE		0
+#define	BLOCKED		1
+
+#define UNFINISHED	0
+#define FINISHED	1
+
+#define INFINITY	0xFFFFFFFF
+
+#define REVERSE_COST	1000		//750*2
+
+typedef struct routeServerData{
+	track_node trackA[TRACK_MAX];
+	int	blockedTrackNodes[TRACK_MAX];	
+}routeServerData;
+
+typedef struct DijkstraEntry{
+	int v;			//vertex
 	
-// 	while (1)
-// 	{
-// 		Receive(&requester, &req, sizeof(routeServerRequest));
+	int isFinished;	
+	
+	int D; 		//least_cost:D(v)
+	int p;		//pre_node:p(v)
+}DijkstraEntry;
+
+
+static void
+initRouteServerData(routeServerData* rtSvrData){
+	int i;
+	
+	init_tracka(rtSvrData->trackA);
+	
+	for (i = 0; i < TRACK_MAX; i++)
+	{
+		rtSvrData->blockedTrackNodes[i] = FREE;
+	}
+	
+}
+
+void
+routeServer(){
+	routeServerData rtSvrData;
+	
+	int requester;
+	routeServerRequest req;
+	
+	initRouteServerData(&rtSvrData);
 		
-// 		switch (req.rtSvrReq_uid)
-// 		{
-// 			case ROUTESERVER_PREDICT_NEXT_SENSOR:
-// 			{	
-// 				int group = req.curValue / 17;
-// 				int id = req.curValue % 17;
-// 				int index = (id + (group - 'A') * 16) - 1;
 
-// 				track_node *nextLandmark = trackA[index].edge[DIR_AHEAD].dest;
-// 				int totalDist = trackA[index].edge[DIR_AHEAD].dist;
+	while (1)
+	{
+		Receive(&requester, &req, sizeof(routeServerRequest));
+		
+		switch (req.rtSvrReq_uid)
+		{
+			case ROUTESERVER_ROUTE_GET_SHORTEST:
+			{	
+				DijkstraEntry dests[TRACK_MAX];
+				
+				int src  = req.src;
+				int dest = req.dest;
 
-// 				while(nextLandmark->type != NODE_SENSOR && nextLandmark->type != NODE_EXIT){
-// 					int direction = DIR_AHEAD;
-// 					if(nextLandmark->type == NODE_BRANCH){
-// 						int status = getSwitchStatus(nextLandmark->num);
-// 						switch(status){
-// 							case STRAIGHT:
-// 								direction = DIR_STRAIGHT;
-// 								break;
-// 							case CURVED:
-// 								direction = DIR_CURVED;
-// 								break;
-// 						}
-// 					}
-// 					totalDist += nextLandmark->edge[direction].dist;
-// 					nextLandmark = nextLandmark->edge[direction].dest;
-// 				}
+				int firstReverseFree = 1;
+				
+				int cost, isReverse;
+				
+				
+				/*
+				 * Dijkstra's Algo: Init Step
+				 */ 															
+				int i;
+				for (i = 0; i < TRACK_MAX; i++)
+				{
+					dests[i].v = i;
+					dests[i].isFinished = UNFINISHED;
+					
+					if(isNeighbor(&(rtSvrData.trackA[i]), &(rtSvrData.trackA[src]), &cost, &isReverse))
+					{
+						if (firstReverseFree == 1 && isReverse == 1){
+							dests[i].D = 0;
+							dests[i].p = src;	
+							
+							firstReverseFree == 0;
+						}
+						else{
+							dests[i].D = cost;
+							dests[i].p = src;								
+						}						
+					}
+					else
+					{
+						dests[i].D = INFINITY;
+						dests[i].p = -1;							
+					}					
+				}
+				
+				dests[src].isFinished = FINISHED; 		
+						
+				/*
+				 * Dijkstra's Algo: Main loop
+				 */ 
+				int mineNodeIdx;
+				while(1)
+				{
+					mineNodeIdx = findMin(dests);
+					
+					dests[mineNodeIdx].isFinished = 1;
+					
+					if(mineNodeIdx = dest || mineNodeIdx = -1)
+						break;
+					
+					if (rtSvrData.blockedTrackNodes[mineNodeIdx] == BLOCKED)
+						continue;						
+														
+					for (i = 0; i < TRACK_MAX; i++)
+					{						
+						if (dests[i].isFinished == UNFINISHED && 
+								isNeighbor(&(rtSvrData.trackA[i]), &(rtSvrData.trackA[mineNodeIdx]), &cost, &isReverse))
+						{
+							if(dests[mineNodeIdx].D + cost < dests[i].D){
+								dests[i].D = dests[mineNodeIdx].D + cost;
+								dests[i].p = dests[mineNodeIdx].v;
+							}								
+						}						
+					}					
+				}	
+				
+				int a = 0;
+				if (mineNodeIdx != -1){
+					int curIdx = mineNodeIdx;
+					while(curIdx != src){
+						sprintf(COM2, "%s\033[45;%uH%d%s", 
+							save, a, curIdx, restore);
+						a = a + 6;
+						curIdx = dests[curIdx].p;
+					}
+				}
+				
+				
+				
+				Reply(requester, NULL, 0);			
+				break;		
+			}			
+		}
+	}
+}
 
-// 				nextSensorInfo response;
-// 				response.nextSensorLabel = nextLandmark->name;
-// 				response.dist = totalDist;
 
-// 				Reply(requester, &response, sizeof(nextSensorInfo));
-// 				break;
-// 			}			
-// 		}
-// 	}
-// }
+/*
+ *	DijkstraEntry related functions
+ */
 
-// nextSensorInfo
-// predictNextSensor(int curSensor, int ts){
-// 	routeServerRequest req;
+static int 
+NodeToIdx(track_node* track, track_node* src){
+	return track - src;
+}
 
-// 	req.rtSvrReq_uid = ROUTESERVER_PREDICT_NEXT_SENSOR;
-// 	req.curValue = curSensor;
-// 	req.ts = ts;
+static int
+isNeighbor(track_node* src, track_node* dest, int* cost, int* isReverse){	//can src link to dest?
+	switch (src->type)
+	{
+		case NODE_BRANCH:
+			//if (src->edge[DIR_STRAIGHT].dest == dest || src->edge[DIR_CURVED].dest == dest || src->reverse == dest)
+			//	return 1;
+			if (src->edge[DIR_STRAIGHT].dest == dest){
+				*cost = src->edge[DIR_STRAIGHT].dist;
+				return 1;
+			}
+			else if (src->edge[DIR_CURVED].dest == dest){
+				*cost = src->edge[DIR_CURVED].dist;
+				return 1;
+			}
+			else if (src->reverse == dest){
+				*cost = REVERSE_COST;
+				*isReverse = 1;
+				return 1;
+			}
+			
+			break;
+		case NODE_SENSOR:
+		case NODE_ENTER:
+		case NODE_MERGE:
+			//if (src->edge[DIR_AHEAD].dest == dest || src->reverse == dest)
+			//	return 1;
+			if (src->edge[DIR_AHEAD].dest == dest){
+				*cost = src->edge[DIR_AHEAD].dist;
+				return 1;
+			}
+			else if (src->reverse == dest){
+				*cost = REVERSE_COST;
+				*isReverse = 1;
+				return 1;
+			}
+			break;
+		case NODE_EXIT:
+			if (src->reverse == dest){
+				*cost = REVERSE_COST;
+				*isReverse = 1;
+				return 1;
+			}
+			break;			
+	}
 
-// 	nextSensorInfo retval;
+	return 0;
+}
 
-// 	Send(ROUTESERVER_TID, &req, sizeof(routeServerRequest), &retval, sizeof(nextSensorInfo));
+static int
+findMin(DijkstraEntry* dests){	
+	int retval = -1;
+	int minCost = INFINITY;
+	
+	int i;
+	for (i = 0; i < TRACK_MAX; i++)
+	{
+		if(dests[i]->isFinished == 1)
+			continue;
+		else if(dests[i]->D < minCost){
+			minCost = dests[i]->D;
+			retval = dests[i]->v;
+		}
+	}
+	
+	return retval;
+}
 
-// 	return retval;
-// }
+
+
