786745914601c9fd0cfae6fc663834c1979635c1
y363liu@gmail.com <y363liu@y363liu-VirtualBox.(none)>
2014-07-10 19:44:50 -0400

    YangChange

--- src/train/routeServer.c
+++ src/train/routeServer.c
@@ -145,7 +145,7 @@ routeServer(){
 				
 				int cost, isReverse;
 				
-				sprintf(COM2, "%s\033[40;0%d%s", 
+				sprintf(COM2, "%s\033[40;0H%d%s", 
 					save, 0, 699, restore);				
 				/*
 				 * Dijkstra's Algo: Init Step
