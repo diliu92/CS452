ab5250a12583a7313e3f0f9beaf265c711605bc3
y363liu@gmail.com <y363liu@y363liu-VirtualBox.(none)>
2014-07-17 13:24:29 -0400

    YangChange

--- src/train/trackServer.c
+++ src/train/trackServer.c
@@ -711,7 +711,7 @@ trackServer(){
 							
 							//TODO: if (nextNode == thisNode->reverse)
 							if (nextNode->num == dest){
-								int totalDist  = currentDist - 649;
+								int totalDist  = currentDist;// - 649;
 								int totalDelay = (totalDist * 10000 / 48900);
 								
 								sprintf(COM2, "%s\033[46;0H%d%s", 
