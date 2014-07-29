a2856ba2e5828507f1ed047c68d84dd2f3364672
y363liu@gmail.com <y363liu@y363liu-VirtualBox.(none)>
2014-07-29 14:05:01 -0400

    YangChange

--- src/train/trackServer.c
+++ src/train/trackServer.c
@@ -625,13 +625,9 @@ trackServer(){
 						}
 						
 						trainStatus* thisTrainStat = &(trkSvrData.trainsStatus[i]);		
-
-						putc(COM1, 0);
-						putc(COM1, thisTrainStat->trainNum);																							
+																							
 						putc(COM1, 15);
-						putc(COM1, thisTrainStat->trainNum);	
-						putc(COM1, 0);
-						putc(COM1, thisTrainStat->trainNum);					
+						putc(COM1, thisTrainStat->trainNum);						
 									
 						putc(COM1, 10);
 						putc(COM1, thisTrainStat->trainNum);						
