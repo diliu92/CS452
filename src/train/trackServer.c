07d626acf0fa2d6ae0337aa8f66c9fcc6f193017
y363liu@gmail.com <y363liu@y363liu-VirtualBox.(none)>
2014-07-28 21:12:49 -0400

    YangChange

--- src/train/trackServer.c
+++ src/train/trackServer.c
@@ -637,9 +637,9 @@ trackServer(){
 						putc(COM1, 0);
 						putc(COM1, thisTrainStat->trainNum);
 					
-						//(trkSvrData.initTrainNum)--;
+						(trkSvrData.initTrainNum)--;
 						
-						req.value = req.value - 1;
+						//req.value = req.value - 1;
 					}
 					else if(	req.value == 	(5 - 1) 	//A5
 							|| 	req.value == 	(8 - 1) 	//A8
